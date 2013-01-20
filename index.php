37d2c61da4f78cf55cb62da300d6d34f4dadca96
Durgesh suthar <durgesh.30@rediffmail.com>
2013-01-20 09:42:57 +0530

    Different dropdowns for different type of file

--- index.php
+++ index.php
@@ -3,23 +3,43 @@
 <head>  
     <meta charset="UTF-8" />  
     <title>HTML5 File API</title>  
-    <link rel="stylesheet" href="style.css" />  
+    <link rel="stylesheet" href="style.css" /> 
+    <SCRIPT>
+
+        var image = new Array("jpg","png","gif","bmp","ico");
+        var audio = new Array("mp3", "aac", "wav","ogg");
+        var video = new Array("3gp", "avi", "flv","mp4","mp3","mkv","wmv");
+
+        function swapOptions(ArrayName){
+            var ExSelect = document.theForm.examples;
+            var theArray = eval(ArrayName);
+            setOptionText(ExSelect, theArray);
+        }
+
+        function setOptionText(theSelect, theArray){
+            for (loop = 0; loop < theSelect.options.length; loop++){
+                theSelect.options[loop].text = theArray[loop];
+            }
+        }
+    </SCRIPT>
+ 
 </head>  
 <body>  
     <div id="main">  
         <h1>Upload Your File</h1>  
-        <form method="post" enctype="multipart/form-data"  action="#">  
+        <form method="post" enctype="multipart/form-data" name="theForm" action="#">  
               
-    TYPE:   <select class="type">
-                <option class="opt" name="image">image</option>
+    TYPE:   <select name="chooseCat" class="type" ONCHANGE="swapOptions(this.options[selectedIndex].text);" size="1">
+                <option selected="selected" class="opt" name="image">image</option>
                 <option class="opt" name="audio">audio</option>
                 <option class="opt" name="video">video</option>
             </select>
-        TO: <select class ="to">
-                <option class="opt2" name="png">png</option>
+        TO: <select class ="to" size="1" name="examples">
                 <option class="opt2" name="jpg">jpg</option>
+                <option class="opt2" name="png">png</option>
                 <option class="opt2" name="gif">gif</option>
-                <option class="opt2" name="mp3">mp3</option>
+                <option class="opt2" name="bmp">bmp</option>
+                <option class="opt2" name="ico">ico</option>
             </select>
             <input type="file" name="upload_file" id="upload_file" />
             <button type="submit" id="btn">Convert File!</button>  
